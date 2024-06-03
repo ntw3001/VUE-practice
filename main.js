2571937dfd8d258f2164d944d065d50c10c15f01
Nick Westbrook <ntw3001@gmail.com>
2024-06-04 00:37:25 +0900

    added final puzzle

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const instance = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg'
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.myminifactory.com/'
         }
     }
 })
