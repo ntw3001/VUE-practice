24a6bc48cd761c33d7b9a609d1fb0ab9e0962082
Nick Westbrook <ntw3001@gmail.com>
2024-06-04 23:43:23 +0900

    added some lists using v-for

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
-        }
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['planck length', 'microscopic', 'miniscule', 'titchy', 'teeny-weeny', 'tiny', 'little', 'small', 'boring', 'bigger than that', 'large', 'great', 'vast', 'huge', 'gigantic', 'colossal', 'brobdingagian', 'gargantuan', 'to stand astride the very Earth itself'],
+            variants: [
+              {id: 2234, colour: "green"},
+              {id: 2235, colour: "blue"}
+            ]
+          }
     }
 })
