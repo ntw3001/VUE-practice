987b32c41fbf795d1018d8f3bbb3409381f13898
Nick Westbrook <ntw3001@gmail.com>
2024-08-05 23:01:06 +0900

    added a local custom directive

--- src/components/directives/index.vue
+++ src/components/directives/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div v-cooldir:red.big="textToRender"></div>
+  <div v-user-last-Name="lastName"></div>
   <button @click="textToRender = 'Don\'t use such language'">
     Update
   </button>
@@ -8,5 +9,12 @@
 <script setup>
   import { ref } from "vue";
     const textToRender = ref("Fuck! What a silly word that is")
+    const lastName= ref("Jones")
 
+    const vUserLastName = {
+      beforeMount(el, binding){
+        el.innerHTML = binding.value;
+        el.style.color = "red";
+      }
+    }
   </script>
