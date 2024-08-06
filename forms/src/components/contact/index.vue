3a671cf0bfeb5accd03f87464cac15a484c3146a
Nick Westbrook <ntw3001@gmail.com>
2024-08-06 23:18:48 +0900

    a bit more on forms

--- forms/src/components/contact/index.vue
+++ forms/src/components/contact/index.vue
@@ -4,44 +4,48 @@
         <div class="col-xl-12">
           <h1>Contact us</h1>
           <hr />
-  
+
           <div class="mb-3">
             <label for="name">Name</label>
             <input
               type="text"
               id="name"
               class="form-control"
+              v-model="formData.name"
             />
           </div>
-      
+
           <div class="mb-3">
             <label for="email">Email</label>
             <input
               type="email"
               id="email"
               class="form-control"
+              v-model="formData.email"
             />
           </div>
-  
+
           <div class="mb-3">
             <label for="subject">Subject</label>
             <input
               type="text"
               id="subject"
               class="form-control"
+              v-model="formData.subject"
             />
           </div>
-  
+
           <div class="mb-3">
             <label for="message">Message</label>
             <textarea
               class="form-control"
               rows="3"
               id="message"
+              v-model="formData.message"
             ></textarea>
           </div>
-  
-  
+
+
           <div class="mb-3">
               <h5>Want more spam ? </h5>
               <div class="form-check">
@@ -59,7 +63,7 @@
                   </label>
               </div>
            </div>
-  
+
            <div class="mb-3">
               <h5>What are you ? </h5>
               <div class="form-check">
@@ -77,14 +81,15 @@
                   </label>
               </div>
           </div>
-            
+
             <button
                 class="btn btn-primary"
+                @click.prevent="submitForm"
             >
             Submit
             </button>
-  
-         
+
+
         </div>
       </div>
     </form>
@@ -92,5 +97,20 @@
 
 
 <script setup>
+import{ reactive } from 'vue'
+
+const formData = reactive({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+    newsletter: false,
+    promotions: false,
+    origin: ''
+})
+
+const submitForm = () => {
+    console.log(JSON.stringify(formData))
+}
 
 </script>
