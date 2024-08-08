5c1edb75a7d7a7a092b09e880183aebeb1c5b6eb
Nick Westbrook <ntw3001@gmail.com>
2024-08-08 23:55:56 +0900

    did some error messages on the form

--- forms/src/components/validate/index.vue
+++ forms/src/components/validate/index.vue
@@ -1,17 +1,22 @@
 <template>
+  <h3>No Homers Club</h3>
   <Form>
     <div class="mb-3">
       <label for="name">Name</label>
       <Field
         name='name'
-        :rules="isRequired"
+        :rules="[isRequired, noHomers]"
         placeholder="Enter your name"
         class="form-control"
         />
-      <ErrorMessage name="name"/>
+      <ErrorMessage name="name" as="div" v-slot="{message}">
+        <div class="alert alert-danger" role="alert">
+          {{message}}
+        </div>
+      </ErrorMessage>
     </div>
 
-    <button class="btn btn-primary">
+    <button class="btn btn-primary" type="submit">
       Submit
     </button>
   </Form>
@@ -22,11 +27,16 @@ import { Field, Form, ErrorMessage } from 'vee-validate';
 
 const isRequired=(value)=>{
   if(!value){
-    return 'This field is required.';
-  } else if (value === "Colin"){
-    return 'Colin is not allowed.';
+    return 'This field is required.'
   }
   return true;
+}
+
+const noHomers = (value) => {
+  if (value === 'Homer') {
+    return 'It says no Homers'
+  }
+    return true;
 }
 
 </script>
