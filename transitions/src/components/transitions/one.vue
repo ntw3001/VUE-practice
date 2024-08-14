633855de885c8e4e301e957e1c37f68b18f2cbbb
Nick Westbrook <ntw3001@gmail.com>
2024-08-14 22:11:12 +0900

    started working with transitions#

---
+++ transitions/src/components/transitions/one.vue
@@ -0,0 +1,55 @@
+<template>
+  <transition>
+    <div class="p-3 mb-2 bg-success text-white"
+        v-if="data.display"
+    >    Hello
+    </div>
+
+  </transition>
+    <button class="btn btn-primary"
+        @click="data.display = !data.display"
+    >
+        Toggle
+    </button>
+
+    <hr/>
+
+    <transition>
+    <div class="p-3 mb-2 bg-success text-white"
+        v-if="data.display2"
+    >    Hello
+    </div>
+
+  </transition>
+    <button class="btn btn-primary"
+        @click="data.display2 = !data.display2"
+    >
+        Toggle Display 2
+    </button>
+</template>
+
+<script setup>
+   import { reactive } from 'vue';
+
+   const data = reactive({
+    display:false
+   })
+</script>
+
+<style scoped>
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
+
+
+  .v-enter-active,
+  .v-leave-active {
+    transition: 1s;
+  }
+
+  .v-leave-from,
+  .v-enter-to {
+    opacity: 1;
+  }
+</style>
