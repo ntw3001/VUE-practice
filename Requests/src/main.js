7627776264da7642a4956adfaf3da07a959f4fc6
Nick Westbrook <ntw3001@gmail.com>
2024-08-22 13:18:26 +0900

    improved the product by incorporating toast

--- Requests/src/main.js
+++ Requests/src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'; // Correct path to App.vue
 import Header from './components/header/Header.vue'; // Correct path to Header.vue
 import Loader from './components/utils/Loader.vue'; // Correct path to Loader.vue
+import ToastPlugin from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-bootstrap.css';
 
 const app =  createApp(App);
 
 app.component('app-header',Header);
 app.component('app-loader',Loader);
+app.use(ToastPlugin);
 app.mount('#app')
