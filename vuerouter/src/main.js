eaa3a16e66d1dd1044bd46471d960b274456d279
Nick Westbrook <ntw3001@gmail.com>
2024-08-29 22:52:17 +0900

    added router section

---
+++ vuerouter/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import Header from './components/header/index.vue';
+
+const app =  createApp(App);
+
+app.component('app-header',Header);
+app.mount('#app')
