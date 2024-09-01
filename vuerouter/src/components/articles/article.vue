e1d402aafaf29cf382dd154b8a877fac71c26e5d
Nick Westbrook <ntw3001@gmail.com>
2024-09-01 23:48:05 +0900

    added dynamic ID for routing

--- vuerouter/src/components/articles/article.vue
+++ vuerouter/src/components/articles/article.vue
@@ -59,3 +59,12 @@
     </div>
   </div>
 </template>
+
+<script setup>
+
+import { useRoute } from 'vue-router'
+
+const route = useRoute();
+console.log(route.params);
+
+</script>
--- vuerouter/src/router/index.js
+++ vuerouter/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Articles from '../components/articles/index.vue';
 import Contact from '../components/contact/index.vue';
 import Home from '../components/home.vue';
+import Article from '../components/articles/article.vue';
 
 const router = createRouter ({
   history: createWebHistory(),
   routes: [
     { path: '/articles', component: Articles },
+    { path: '/articles/:articleID', component: Article },
     { path: '/contact', component: Contact },
     { path: '/', component: Home }
   ]
