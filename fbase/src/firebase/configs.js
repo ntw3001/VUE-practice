76260969999a15b1b9b40e7da8b71b925367505b
Nick Westbrook <ntw3001@gmail.com>
2024-09-13 23:53:52 +0900

    inserted firebase/firestore, haven't done anything with it yet

---
+++ fbase/src/firebase/configs.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB8lzPjUp1g4cyuEDSq1ktIdQB1Xd7msjI",
+  authDomain: "just-a-bit-of-vue-practice.firebaseapp.com",
+  projectId: "just-a-bit-of-vue-practice",
+  storageBucket: "just-a-bit-of-vue-practice.appspot.com",
+  messagingSenderId: "729876504191",
+  appId: "1:729876504191:web:a7b3ee051dc5158879e94a",
+};
+
+initializeApp(firebaseConfig);
+
+const DB = getFirestore();
+export  { DB };
