eadbaa81c8779e03bae50767639c1886811b4aa4
Nick Westbrook <ntw3001@gmail.com>
2024-09-18 00:06:45 +0900

    finished fishing up documents from firebase

--- fbase/src/components/home.vue
+++ fbase/src/components/home.vue
@@ -2,41 +2,11 @@
     <div class="row row-cols-1 row-cols-md-2 g-4">
 
     <!-- ITEM -->
-    <div class="col">
+    <div class="col" v-for = "(note) in notes">
         <div class="card">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-        </div>
-        </div>
-    </div>
-
-    <!-- ITEM -->
-    <div class="col">
-        <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-        </div>
-        </div>
-    </div>
-
-    <!-- ITEM -->
-    <div class="col">
-        <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-        </div>
-        </div>
-    </div>
-
-    <!-- ITEM -->
-    <div class="col">
-        <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+            <h5 class="card-title">{{ note.title }}</h5>
+            <p class="card-text">{{  note.description }}</p>
         </div>
         </div>
     </div>
